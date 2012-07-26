ea46316e3e1117419ca7ba672b58c7addecec4b0
Tiago Sarri <titisarri@gmail.com>
2012-07-26 14:52:55 -0300

    Config do mysql e devise

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module RailsDeviceLoginFacebook
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    config.i18n.default_locale = 'pt-BR'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
