8d3a8b9fed78c5a92af69d4f7f87424d510cc771
Tiago Sarri <titisarri@gmail.com>
2012-07-26 13:14:22 -0300

    Primeiro commit do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDeviceLoginFacebook::Application.config.secret_token = 'bd9bb10dd067d3c45f1cbe1e90716f04ef36f559ddd938e47fdb26fa88654388cb46e447150bcba847800b51fda9c9f4965f3077eb5835c268f20c3e48cc0c64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDeviceLoginFacebook::Application.config.session_store :cookie_store, :key => '_rails-device-login-facebook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsDeviceLoginFacebook::Application.config.session_store :active_record_store
